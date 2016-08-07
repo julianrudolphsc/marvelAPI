28c645b39e0524b0b71f798d28d3398478b1e7d3
julian rudolph <julian.rudolphsc@gmail.com>
2016-08-07 05:50:11 -0600

    added jq, and others

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var request = require('request');
+var $ = require('jquery');
 
 
 app.use(express.static(__dirname + '/public'));
@@ -9,6 +11,13 @@ app.set('view engine', 'ejs');
 //home route
 app.get('/', function(req, res){
   res.render('home');
+});
+
+app.get('/getHero', function(req, res){
+  var myUrl = "http://gateway.marvel.com:80/v1/public/characters/";
+  var myKey = "4337f0a429d61d2d987288de6cbf339e";
+  var charName = $('input[type="text"]').val();
+  console.log('At getHero');
 });
 
 //listener
